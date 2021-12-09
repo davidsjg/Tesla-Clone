ce4661629b04a4af6e7df2c77972832cc513f8aa
Joey Davidson <j.davidson18@gmail.com>
2021-12-09 13:18:30 -0700

    heroku deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+
 import "./App.css";
 import Header from "../src/components/Header/Header";
 import Home from "./components/Home/Home";
